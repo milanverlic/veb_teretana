b7df96b087ea654772b696f274a1eb87c785c306
milanverlic <verlicmilan786@gmail.com>
2026-05-26 21:29:09 +0200

    Inicijalizacija arhitekture, React Router i Bootstrap

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header>FitFlow Navigacija</header>;
+};
+
+export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Učitavamo Bootstrap stilove
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+// Ovde ćemo definisati koje putanje vode do kojih stranica
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* Kasnije ćemo ovde dodati <Route path="/login" element={<LoginScreen />} /> itd. */}
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
